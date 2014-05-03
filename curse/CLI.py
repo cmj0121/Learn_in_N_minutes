da507a39118eb4211412e4b2c66d2d503617291f
cmj0121 <cmj0121@gmail.com>
2014-05-03 15:59:52 +0800

    [CURSES] Add Scenario 2
    	Shows how to setup three panel, setup the background color and
    	enable/disable user input

--- curse/CLI.py
+++ curse/CLI.py
@@ -14,32 +14,79 @@ class CLI(object):
 
 	>>> cli.workspace()
 	"""
-	def __init__(self, border=False, cursor=True):
+	def __init__(self, border=False, cursor=True, keypad=1):
 		scr = curses.initscr()
 		curses.noecho()
 
 		if border: scr.box()
 		curses.curs_set(cursor)
+		scr.keypad(keypad)
 		scr.refresh()
+		curses.mousemask(curses.ALL_MOUSE_EVENTS)
+		self.msg = ""
 
 		self.scr = scr
+	def __del__(self):
+		self.scr.keypad(0)
+	def buildPanel(self, frame=0, border=False):
+		""" Show the three windows as
+			+----+--------------------+
+			|    |                    |
+			|    |                    |
+			|    |         2          |
+			| 1  |                    |
+			|    |                    |
+			|    |                    |
+			|    +--------------------+
+			|    |         3          |
+			+----+--------------------+
+		"""
+		y, x = self.scr.getmaxyx()
+
+		## Left Slidebar, width = ScreenWidth*0.2
+		leftSlideBar = curses.newwin(y, int(x*0.2), 0, 0)
+		## WorkSpace, height = ScreenHeight*0.8
+		workSpace = curses.newwin(int(y*0.8), x-int(x*0.2), 0, int(x*0.2))
+		## FootBar
+		footBar = curses.newwin(y-int(y*0.8), x-int(x*0.2), int(y*0.8), int(x*0.2))
+
+		ret = (leftSlideBar, workSpace, footBar)
+		curses.start_color()
+		curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLUE)
+		curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+		curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
+		ret[frame].bkgd(curses.color_pair(frame+1))
+
+		[_.box() for _ in ret]
+		[ret[_].addch(1, 1, str(_)) for _ in range(len(ret))]
+		footBar.addstr(" %s "%self.msg)
+		[_.refresh() for _ in ret]
+		return ret
 	def loopMain(self, pos=(8, 10)):
-		win = curses.newwin(3, 20, pos[0], pos[1])
-		win.box()
-		win.refresh()
+		cur = 3
+		_, main, _ = self.buildPanel(cur%3)
 		while True:
 			ch = self.scr.getch()
 			if 0x71 == (ch | 0x20): break
+			elif curses.KEY_UP == ch:
+				if cur == 2: cur = 1
+				elif 0 == cur%3: cur = 3
+			elif curses.KEY_DOWN == ch:
+				if cur == 1: cur = 2
+				elif 0 == cur%3: cur = 6
+			elif curses.KEY_RIGHT == ch:
+				if 0 == cur%3: cur = cur/3
+			elif curses.KEY_LEFT == ch:
+				if 0 != cur%3: cur = cur*3
+			elif 2 == cur%3:
+				if 0x7F == ch: self.msg = self.msg[:-1]
+				else: self.msg =  self.msg + chr(ch)
+			_, main, _ = self.buildPanel(cur%3)
+			main.refresh()
 
-			msg = "Key: %s" %ch
-			if chr(ch) in string.printable: msg += " (%s)" %chr(ch)
-			win.clear()
-			win.box()
-			win.addstr(1, 1, msg)
-			win.refresh()
 if __name__ == "__main__":
 	try:
-		cli = CLI(border=True, cursor=False)
+		cli = CLI(border=False, cursor=False)
 		cli.loopMain()
 	except Exception as e:
 		print e
--- curse/README.md
+++ curse/README.md
@@ -23,10 +23,34 @@ on the screen. Also, we need to `refresh` to force show data into
 screen.
 
 Conclusion:
-	1. Initial your curses by `initscr`.
-	2. Setup your curses for `echo`, `curs_set` ... etc.
-	3. Update your screen by `refresh`.
-	4. Receive user's input by `getch`.
-	5. Clean-up all setting for your curses, otherwise your bash will
-		be polluted.
+1. Initial your curses by `initscr`.
+2. Setup your curses for `echo`, `curs_set` ... etc.
+3. Update your screen by `refresh`.
+4. Receive user's input by `getch`.
+5. Clean-up all setting for your curses, otherwise your bash will be polluted.
+
+
+## Scenario 2
+	Build a window which have three panels. You can switch to each one via
+	arrow key. And more, only one panel can type and show messages you
+	input when this panel is focused.
+
+
+In this scenraio, we need to create three panels and each panel controls their
+background color. We can use `curses.init_pair` to setup the color-pair, and
+it uses the alias number which you defined to setup the background color.
+Before setup the background, we need to create three panels via `curses.win`
+which return a panel object (exactly, it is window object) and we can setup
+the background color.
+
+After create the necessary widget, we need to control our logic via `getch`
+which return each KeyCode we press. If we press the arrow keys, program will
+resketch the screen and program will cache which panel is focused and display
+the background color. When the footer bar is focused, it will enable user
+to type any string via we cache the string and shows the string.
+
+Conclusion:
+1. Create three panel via `curses.win`
+2. Set the background via `bkgd` which setup color pair by `curses.init_pair`
+3. Control logic by `getch` and resketch screen
 
